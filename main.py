c966c096854b79dd3fe22aa8fcced10b11789642
maia <nightisshortg@gmail.com>
2023-02-23 15:29:37 -0300

    scr

--- main.py
+++ main.py
@@ -5,29 +5,41 @@ import collections
 Pattern = collections.namedtuple('Pattern', ['LogInput', 'ReplayOutput'])
 Pokemon = collections.namedtuple('Pokemon', ['Health', 'PokemonName', 'PokemonType'])
 
-def PatternReplacement(match):
-    return f'(?P<{match.group(1)}>.+?)'
-
 class LogToReplayCoverter:
     def __init__(self):
         self.GameInfo = dict()
         self.Lang = list()
         for el in json.load(open("lang.json")):
             self.Lang.append(Pattern(
-                re.sub(r'\{(\w+)\}', PatternReplacement, el["Log"]),
-                None
+                re.sub(
+                    r'\{(\w+)\}',
+                    lambda match: f'(?P<{match.group(1)}>.+?)',
+                    el["Log"]
+                ), # converter json em regex pra busca
+                el["Replay"] if "Replay" in el else None
             ))
     
     def ParseLine(self, line:str):
+        print(line.rstrip("\n"))
         for pat in self.Lang:
             match = re.match(pat.LogInput, line)
             if match is not None:
                 for k, v in match.groupdict().items(): # dict com todas as informações
                     self.GameInfo[k] = v
                 print(self.GameInfo)
-                return
-        print("Couldn't find ")
-                    
+                return re.sub(
+                    r'\{(\w+)\}',
+                    lambda match: self.GameInfo[match.group(1)],
+                    pat.ReplayOutput
+                ) if pat.ReplayOutput else None
+        print(f"Couldn't find")
+
+    def ConvertLog(self, path:str):
+        with open(path, "r") as logFile:
+            log = logFile.readlines()
+        for line in log:
+            if line == "\n": continue
+            print(self.ParseLine(line), end="\n\n")
 
 LTR = LogToReplayCoverter()
-LTR.ParseLine("Start of Turdsadan 5a")
+LTR.ConvertLog(r"examples/logs/BuRnXN0t.txt")
